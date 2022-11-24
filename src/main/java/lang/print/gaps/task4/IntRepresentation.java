91715548871b50d3056fc577dd9ac40eac89a8ff
hetot <shaxzod.murtazayev@mail.ru>
2022-11-24 19:20:02 +0300

    done

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        byte c = 99;
+        byte h = 104;
+        byte a = 97;
+        byte r = 114;
+
+        System.out.println((char)c);
+        System.out.println((char)h);
+        System.out.println((char)a);
+        System.out.println((char)r);
     }
 }
